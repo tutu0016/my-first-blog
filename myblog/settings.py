02f5d37c03bbe37b8eb8b1a07ca47f19578b33a1
tutu0016 <38122617@qq.com>
2018-05-19 02:14:20 +0000

    deleted repeated my-first-blog; modify setting.py

--- myblog/settings.py
+++ myblog/settings.py
@@ -119,14 +119,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
-<<<<<<< HEAD
-STATIC_ROOT = os.path.join(os.path.dirname(__file__),'static')
-STATICFILES_DIRS = (
-    ('css',os.path.join(STATIC_ROOT,'css').replace('\\','/') ),
-    ('js',os.path.join(STATIC_ROOT,'js').replace('\\','/') ),
-    ('images',os.path.join(STATIC_ROOT,'images').replace('\\','/') ),
-    ('upload',os.path.join(STATIC_ROOT,'upload').replace('\\','/') ),
-)
-=======
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
->>>>>>> 1130d6ab1865bbc527921159bbb2415200b426c1
